1ce551d20446ea452c62936fda133cc2baf7fd01
jiikko <n905i.1214@gmail.com>
2021-05-08 09:59:16 +0900

    killedにも反応できるようにする

--- app/javascript/channels/matching_channel.js
+++ app/javascript/channels/matching_channel.js
@@ -3,16 +3,16 @@ import consumer from "./consumer"
 const matching_data = document.getElementById('matching-data')
 const matchingId = matching_data && matching_data.dataset.matchingId;
 if(matchingId) {
-  const enemy1Sound = document.querySelector("#enemy1_respawn");
-  const enemy2Sound = document.querySelector("#enemy2_respawn");
-  const enemy3Sound = document.querySelector("#enemy3_respawn");
-  const enemy4Sound = document.querySelector("#enemy4_respawn");
+  window.enemy1Sound = document.querySelector("#enemy1_respawn");
+  window.enemy2Sound = document.querySelector("#enemy2_respawn");
+  window.enemy3Sound = document.querySelector("#enemy3_respawn");
+  window.enemy4Sound = document.querySelector("#enemy4_respawn");
 
   async function playSound(action, enemy_no) {
     try {
-      if(data.action == "respawn") {
-        await this[`enemy${enemy_no}Sound`].play();
-      } else if(data.action == "killed") {
+      if(action == "respawn") {
+        await window[`enemy${enemy_no}Sound`].play();
+      } else if(action == "killed") {
         // TODO
       }
     } catch(err) {
