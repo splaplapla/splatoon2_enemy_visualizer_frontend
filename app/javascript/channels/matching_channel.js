f8373dfd37ea5c72137b18fb214a1b02fb967409
jiikko <n905i.1214@gmail.com>
2021-05-08 14:40:12 +0900

    ファーストコレクションを使う

--- app/javascript/channels/matching_channel.js
+++ app/javascript/channels/matching_channel.js
@@ -1,7 +1,23 @@
 import consumer from "./consumer"
 
+class EnemyRegistry {
+  constructor(enemy) {
+    this.enemies = []
+  }
+  register(enemy) {
+    this.enemies.push(enemy);
+  }
+  find(enemy_no) {
+    return this.enemies.find(enemy => enemy.no() == enemy_no);
+  }
+  enemies() {
+    return this.enemies;
+  }
+}
+
 class Enemy {
   constructor(enemy_no) {
+    this.enemy_no = enemy_no
     this.sound = document.querySelector(`#enemy${enemy_no}_respawn`)
     this.age = 0
     this.status = "live"
@@ -14,6 +30,11 @@ class Enemy {
     this.age = 0;
     this.status = "death";
   }
+  no() {
+    return this.enemy_no
+  }
+  render() {
+  }
   getOld() {
     if(this.status == "live") {
       this.age = this.age + 1;
@@ -25,27 +46,25 @@ const matching_data = document.getElementById('matching-data')
 const matchingId = matching_data && matching_data.dataset.matchingId;
 
 if(matchingId) {
-  window.enemy1 = new Enemy(1);
-  window.enemy2 = new Enemy(2);
-  window.enemy3 = new Enemy(3);
-  window.enemy4 = new Enemy(4);
+  window.registry = new EnemyRegistry
+  registry.register(new Enemy(1))
+  registry.register(new Enemy(2))
+  registry.register(new Enemy(2))
+  registry.register(new Enemy(4))
 
   async function playSound(action, enemy_no) {
     try {
       if(action == "respawn") {
-        await window[`enemy${enemy_no}`].respawn();
+        await registry.find(enemy_no).respawn();
       } else if(action == "killed") {
-        await window[`enemy${enemy_no}`].killed();
+        await registry.find(enemy_no).killed();
       }
     } catch(err) {
       console.log(err);
     }
   }
   function getOld(enemy) {
-    window.enemy1.getOld();
-    window.enemy2.getOld();
-    window.enemy3.getOld();
-    window.enemy4.getOld();
+    window.registry.enemies.forEach(enemy => enemy.getOld())
   }
 
   window.setInterval(getOld, 1000);
